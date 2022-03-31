d2317c0dfdc54a296c8d653dd0d84309d46c127e
unknown <anton.holub91@gmail.com>
2022-03-31 09:33:04 +0300

    new branch 1st commit

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class MainClass {
 
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "D:\\AQA\\Projects\\testselenium\\drivers\\chromedrivernew.exe");
+        System.setProperty("webdriver.chrome.driver", "E:\\QA\\Projects\\testselenium\\drivers\\chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
